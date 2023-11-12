763d6bb724a96f958ec7ae3fada7a95d48299a04
Jaime <quezadajaime@ymail.com>
2023-11-12 06:24:21 -0800

    api index

---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+const thoughtRoutes = require(".thoughtRoutes");
+
+//middleware
+router.use("/users", userRoutes);
+router.use("/thoughts", thoughtRoutes);
+
+//exports
+module.exports = router;
