a3bdf0b1556920536cf4f6f8fd02168de1ff74ad
Jaime <quezadajaime@ymail.com>
2023-11-11 17:36:34 -0800

    reaction

---
+++ models/Reaction.js
@@ -0,0 +1,24 @@
+const { Schema, Types } = require("mongoose");
+
+//This will be used as the reaction field's sub-document schema in the Though
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = reactionSchema;
