98b81a1e69fe8dd0523eb943c54608ffc5d9ef71
Jaime <quezadajaime@ymail.com>
2023-11-19 08:09:51 -0800

    added mongodb for connection

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const { connect, connection } = require("mongoose");
 
 //creates the database
 const connectionString =
-    process.env.MONGODB_URI || ""
+    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/studentsDB";
 
 //connects Mongose and MongoDB
 connect(connectionString);
